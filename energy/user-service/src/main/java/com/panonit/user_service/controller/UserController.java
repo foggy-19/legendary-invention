e65f8ce5866ae596e2b86a25fcd75873853d7862
foggy-19 <milan.pasic@panonit.com>
2026-07-30 15:50:34 +0200

    remove logging statements and add logging aspect for improved request/response tracking

--- energy/user-service/src/main/java/com/panonit/user_service/controller/UserController.java
+++ energy/user-service/src/main/java/com/panonit/user_service/controller/UserController.java
@@ -3,12 +3,10 @@ package com.panonit.user_service.controller;
 import com.panonit.user_service.dto.*;
 import com.panonit.user_service.service.UserService;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-@Slf4j
 @RestController
 @RequestMapping(path = "/api/v1/user")
 @RequiredArgsConstructor
@@ -18,29 +16,21 @@ public class UserController {
 
     @PostMapping
     public ResponseEntity<CreateUserResponseDto> createUser(@RequestBody CreateUserRequestDto request) {
-        log.info("createUser");
-
         return new ResponseEntity<>(service.create(request), HttpStatus.CREATED);
     }
 
     @PutMapping(path = "/{id}")
     public ResponseEntity<UpdateUserResponseDto> updateUser(@PathVariable Long id, @RequestBody UpdateUserRequestDto updateUserRequestDto) {
-        log.info("updateUser");
-
         return ResponseEntity.ok(service.update(id, updateUserRequestDto));
     }
 
     @GetMapping(path = "/{id}")
     public ResponseEntity<GetUserResponseDto> getUser(@PathVariable Long id) {
-        log.info("getUser");
-
         return service.get(id).map(ResponseEntity::ok).orElse(ResponseEntity.notFound().build());
     }
 
     @DeleteMapping(path = "/{id}")
     public ResponseEntity<Void> deleteUser(@PathVariable Long id) {
-        log.info("deleteUser");
-
         service.delete(id);
 
         return ResponseEntity.noContent().build();
--- energy/user-service/src/main/java/com/panonit/user_service/service/impl/UserServiceImpl.java
+++ energy/user-service/src/main/java/com/panonit/user_service/service/impl/UserServiceImpl.java
@@ -6,13 +6,11 @@ import com.panonit.user_service.mapper.UserMapper;
 import com.panonit.user_service.repository.UserRepository;
 import com.panonit.user_service.service.UserService;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
 
-@Slf4j
 @Service
 @RequiredArgsConstructor
 public class UserServiceImpl implements UserService {
@@ -23,8 +21,6 @@ public class UserServiceImpl implements UserService {
     @Override
     @Transactional
     public CreateUserResponseDto create(CreateUserRequestDto createUserRequestDto) {
-        log.info("create {}", createUserRequestDto.getEmail());
-
         if (repository.existsByEmail(createUserRequestDto.getEmail())) {
             throw new IllegalArgumentException("User already exists");
         }
@@ -37,8 +33,6 @@ public class UserServiceImpl implements UserService {
     @Override
     @Transactional
     public UpdateUserResponseDto update(Long id, UpdateUserRequestDto updateUserRequestDto) {
-        log.info("update {}", updateUserRequestDto.getEmail());
-
         User user = repository.findById(id)
                 .orElseThrow(() -> new IllegalArgumentException(String.format("User with ID %s not found", id)));
 
@@ -55,16 +49,12 @@ public class UserServiceImpl implements UserService {
     @Override
     @Transactional
     public Optional<GetUserResponseDto> get(Long id) {
-        log.info("get {}", id);
-
         return repository.findById(id).map(mapper::toGetUserResponseDto);
     }
 
     @Override
     @Transactional
     public void delete(Long id) {
-        log.info("delete {}", id);
-
         User user = repository.findById(id)
                 .orElseThrow(() -> new IllegalArgumentException(String.format("User with ID %s not found", id)));
 
