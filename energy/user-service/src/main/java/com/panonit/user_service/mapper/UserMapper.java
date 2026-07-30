78bc78b52fa0e171a573fb5108846ab645e575e6
foggy-19 <milan.pasic@panonit.com>
2026-07-30 19:19:30 +0200

    remove unused mapping

--- energy/user-service/src/main/java/com/panonit/user_service/mapper/UserMapper.java
+++ energy/user-service/src/main/java/com/panonit/user_service/mapper/UserMapper.java
@@ -1,7 +1,10 @@
 package com.panonit.user_service.mapper;
 
 
-import com.panonit.user_service.dto.*;
+import com.panonit.user_service.dto.CreateUserRequestDto;
+import com.panonit.user_service.dto.CreateUserResponseDto;
+import com.panonit.user_service.dto.GetUserResponseDto;
+import com.panonit.user_service.dto.UpdateUserResponseDto;
 import com.panonit.user_service.entity.User;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
@@ -12,9 +15,6 @@ public interface UserMapper {
 
     @Mapping(source = "notifications", target = "alerting")
     User toEntity(CreateUserRequestDto createUserRequestDto);
-
-    @Mapping(source = "notifications", target = "alerting")
-    User toEntity(UpdateUserRequestDto updateUserRequestDto);
 
     @Mapping(source = "alerting", target = "notifications")
     CreateUserResponseDto toCreateUserResponseDto(User user);
