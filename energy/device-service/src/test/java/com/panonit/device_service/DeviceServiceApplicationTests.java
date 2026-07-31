370bb3344aa0637c2ba79b0465d143448c7260f3
foggy-19 <milan.pasic@panonit.com>
2026-07-31 11:04:51 +0200

    add tests for device and user creation with logging in service application tests

--- energy/device-service/src/test/java/com/panonit/device_service/DeviceServiceApplicationTests.java
+++ energy/device-service/src/test/java/com/panonit/device_service/DeviceServiceApplicationTests.java
@@ -1,13 +1,46 @@
 package com.panonit.device_service;
 
+import com.panonit.device_service.entity.Device;
+import com.panonit.device_service.model.DeviceType;
+import com.panonit.device_service.repository.DeviceRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+@Slf4j
 @SpringBootTest
 class DeviceServiceApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    private static final int NUMBER_OF_DEVICES = 100;
+    private static final int NUMBER_OF_USERS = 10;
 
+    @Autowired
+    private DeviceRepository repository;
+
+    @Test
+    void contextLoads() {
+    }
+
+    @Disabled
+    @Test
+    void createDevices() {
+        log.info("Create devices");
+
+        for (int i = 0; i < NUMBER_OF_DEVICES; i++) {
+            var device = new Device(
+                    null,
+                    "Device " + i,
+                    DeviceType.values()[i % DeviceType.values().length],
+                    "Location " + ((i % 3) + 1),
+                    (long) ((i % NUMBER_OF_USERS) + 1)
+            );
+
+            log.info("Created device {}", device.getName());
+            repository.save(device);
+        }
+
+        log.info("Done");
+    }
 }
--- energy/user-service/src/test/java/com/panonit/user_service/UserServiceApplicationTests.java
+++ energy/user-service/src/test/java/com/panonit/user_service/UserServiceApplicationTests.java
@@ -1,13 +1,46 @@
 package com.panonit.user_service;
 
+import com.panonit.user_service.entity.User;
+import com.panonit.user_service.repository.UserRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+@Slf4j
 @SpringBootTest
 class UserServiceApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    private static final int NUMBER_OF_USERS = 10;
 
+    @Autowired
+    private UserRepository repository;
+
+    @Test
+    void contextLoads() {
+    }
+
+    @Disabled
+    @Test
+    void createUsers() {
+        log.info("Creating users");
+
+        for (int i = 0; i < NUMBER_OF_USERS; i++) {
+            User user = new User(
+                    null,
+                    "First " + i,
+                    "LastName " + i,
+                    "firstname" + i + "@example.com",
+                    "Laze NAN " + i,
+                    i % 2 == 0,
+                    1000.0 + i
+            );
+
+            log.info("Created user {}", user.getEmail());
+            repository.save(user);
+        }
+
+        log.info("Done");
+    }
 }
