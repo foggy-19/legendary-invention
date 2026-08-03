8e953e9092d6f98c686bcc13121fc434162a7da5
foggy-19 <milan.pasic@panonit.com>
2026-08-03 13:47:15 +0200

    add InfluxDB integration for energy usage events

--- energy/usage-service/src/main/java/com/panonit/usage_service/service/UsageService.java
+++ energy/usage-service/src/main/java/com/panonit/usage_service/service/UsageService.java
@@ -1,4 +1,8 @@
 package com.panonit.usage_service.service;
 
+import com.panonit.kafka.event.EnergyUsageEvent;
+
 public interface UsageService {
+
+    void onEnergyUsageEvent(EnergyUsageEvent event);
 }
--- energy/usage-service/src/main/java/com/panonit/usage_service/service/impl/UsageServiceImpl.java
+++ energy/usage-service/src/main/java/com/panonit/usage_service/service/impl/UsageServiceImpl.java
@@ -1,15 +1,43 @@
 package com.panonit.usage_service.service.impl;
 
+import com.influxdb.client.InfluxDBClient;
+import com.influxdb.client.domain.WritePrecision;
+import com.influxdb.client.write.Point;
 import com.panonit.kafka.event.EnergyUsageEvent;
 import com.panonit.usage_service.service.UsageService;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UsageServiceImpl implements UsageService {
 
-    @KafkaListener(topics = "energy-usage", groupId = "usage-service")
-    public void onEnergyUsageEvent(EnergyUsageEvent energyUsageEvent) {
+    private final static String MEASUREMENT = "energy-usage";
+    private final static String TAG = "device-id";
+    private final static String FIELD = "energy-consumed";
 
+    private final String org;
+    private final String bucket;
+    private final InfluxDBClient influx;
+
+    public UsageServiceImpl(
+            @Value("${influx.org}") String org,
+            @Value("${influx.bucket}") String bucket,
+            InfluxDBClient influx
+    ) {
+        this.org = org;
+        this.bucket = bucket;
+        this.influx = influx;
+    }
+
+    @Override
+    @KafkaListener(topics = "energy-usage", groupId = "usage-service")
+    public void onEnergyUsageEvent(EnergyUsageEvent event) {
+        Point point = Point.measurement(MEASUREMENT)
+                .addTag(TAG, event.deviceId().toString())
+                .addField(FIELD, event.consumption())
+                .time(event.timestamp(), WritePrecision.MS);
+
+        influx.getWriteApiBlocking().writePoint(bucket, org, point);
     }
 }
