a5c19157078bcb629d496eabb2b4a2436c9cfe29
foggy-19 <milan.pasic@panonit.com>
2026-08-05 16:05:36 +0200

    add circuit breaker functionality and fallback routes for device, ingestion, insight, and user services

--- energy/gateway/src/main/java/com/panonit/gateway/route/InsightServiceRoutes.java
+++ energy/gateway/src/main/java/com/panonit/gateway/route/InsightServiceRoutes.java
@@ -1,10 +1,14 @@
 package com.panonit.gateway.route;
 
+import org.springframework.cloud.gateway.server.mvc.filter.CircuitBreakerFilterFunctions;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.servlet.function.RequestPredicates;
 import org.springframework.web.servlet.function.RouterFunction;
 import org.springframework.web.servlet.function.ServerResponse;
+
+import java.net.URI;
 
 import static org.springframework.cloud.gateway.server.mvc.filter.BeforeFilterFunctions.uri;
 import static org.springframework.cloud.gateway.server.mvc.handler.GatewayRouterFunctions.route;
@@ -13,12 +17,27 @@ import static org.springframework.cloud.gateway.server.mvc.handler.HandlerFuncti
 @Configuration
 public class InsightServiceRoutes {
 
-    private static final String ID = "insight-service";
+    private static final String ROUTE_ID = "insight-service";
     private static final String URL = "http://localhost:8085";
     private static final String PATH = "/api/v1/insight/**";
 
+    private static final String FALLBACK_ROUTE_ID = "insight-service-fallback";
+    private static final String FALLBACK_PATH = "/fallback/insight";
+    private static final String ERROR_MESSAGE = "Insight service is down";
+
     @Bean
     public RouterFunction<ServerResponse> energyInsightServiceRoute() {
-        return route(ID).route(RequestPredicates.path(PATH), http()).before(uri(URL)).build();
+        return route(ROUTE_ID)
+                .route(RequestPredicates.path(PATH), http())
+                .before(uri(URL))
+                .filter(CircuitBreakerFilterFunctions.circuitBreaker("insightServiceCircuitBreaker", URI.create("forward:" + FALLBACK_PATH)))
+                .build();
+    }
+
+    @Bean
+    public RouterFunction<ServerResponse> energyInsightFallbackRoute() {
+        return route(FALLBACK_ROUTE_ID)
+                .route(RequestPredicates.path(FALLBACK_PATH), request -> ServerResponse.status(HttpStatus.SERVICE_UNAVAILABLE).body(ERROR_MESSAGE))
+                .build();
     }
 }
--- energy/gateway/src/main/java/com/panonit/gateway/route/UserServiceRoutes.java
+++ energy/gateway/src/main/java/com/panonit/gateway/route/UserServiceRoutes.java
@@ -1,10 +1,14 @@
 package com.panonit.gateway.route;
 
+import org.springframework.cloud.gateway.server.mvc.filter.CircuitBreakerFilterFunctions;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.servlet.function.RequestPredicates;
 import org.springframework.web.servlet.function.RouterFunction;
 import org.springframework.web.servlet.function.ServerResponse;
+
+import java.net.URI;
 
 import static org.springframework.cloud.gateway.server.mvc.filter.BeforeFilterFunctions.uri;
 import static org.springframework.cloud.gateway.server.mvc.handler.GatewayRouterFunctions.route;
@@ -13,12 +17,27 @@ import static org.springframework.cloud.gateway.server.mvc.handler.HandlerFuncti
 @Configuration
 public class UserServiceRoutes {
 
-    private static final String ID = "user-service";
+    private static final String ROUTE_ID = "user-service";
     private static final String URL = "http://localhost:8080";
     private static final String PATH = "/api/v1/user/**";
 
+    private static final String FALLBACK_ROUTE_ID = "user-service-fallback";
+    private static final String FALLBACK_PATH = "/fallback/user";
+    private static final String ERROR_MESSAGE = "User service is down";
+
     @Bean
     public RouterFunction<ServerResponse> energyUserServiceRoute() {
-        return route(ID).route(RequestPredicates.path(PATH), http()).before(uri(URL)).build();
+        return route(ROUTE_ID)
+                .route(RequestPredicates.path(PATH), http())
+                .before(uri(URL))
+                .filter(CircuitBreakerFilterFunctions.circuitBreaker("userServiceCircuitBraker", URI.create("forward:" + FALLBACK_PATH)))
+                .build();
+    }
+
+    @Bean
+    public RouterFunction<ServerResponse> energyUserFallbackRoute() {
+        return route(FALLBACK_ROUTE_ID)
+                .route(RequestPredicates.path(FALLBACK_PATH), request -> ServerResponse.status(HttpStatus.SERVICE_UNAVAILABLE).body(ERROR_MESSAGE))
+                .build();
     }
 }
